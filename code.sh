50c1465a9190ccbe56525996dbd9e79bcd6c1881
kiran <kiran@email.com>
2025-06-25 09:46:20 +0000

    codeadd

---
+++ code.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>My Git Project</title>
+</head>
+<body>
+  <h1>Hello, Git!</h1>
+  <p>This is a sample HTML file tracked by Git.</p>
+</body>
+</html>
+
